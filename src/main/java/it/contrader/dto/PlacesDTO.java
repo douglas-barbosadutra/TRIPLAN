c229ab37bd81270abd6cd3ada366de20ace7e6cf
Matteo Faitanini <mfaitamf8@gmail.com>
2019-05-16 15:53:05 +0200

    aggiunta itinerario

--- src/main/java/it/contrader/dto/PlacesDTO.java
+++ src/main/java/it/contrader/dto/PlacesDTO.java
@@ -15,6 +15,9 @@ import lombok.NoArgsConstructor;
 
 public class PlacesDTO {
 
+	private static final double R= 6378.1370D;
+	private static final double CONVERT_DEGREE_TO_RADIANS = Math.PI/180D;
+	
 	private long idPlaces;
 	
 	private String nameplaces;
@@ -28,6 +31,17 @@ public class PlacesDTO {
 	private String type;
 	
 	private CityDTO cityDTO;
+
+	public double distanceTo(PlacesDTO y){
+		double deltaLat = (y.latitude - this.latitude);
+		double deltaLon = (y.longitude - this.longitude);
+		 double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2)
+		            + Math.cos(this.latitude) * Math.cos(y.latitude)
+		            * Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+		 double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		 double distance = R * c * 1000;
+		 return Math.sqrt(distance);
+	}
 
 }
 	
--- src/main/java/it/contrader/service/PlacesService.java
+++ src/main/java/it/contrader/service/PlacesService.java
@@ -1,5 +1,8 @@
 package it.contrader.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import it.contrader.dto.PlacesDTO;
 import it.contrader.model.Places;
@@ -7,6 +10,37 @@ import it.contrader.model.Places;
 
 @Service
 public class PlacesService extends AbstractService<Places, PlacesDTO> {
+	
+	public List<PlacesDTO> getItinerary(List<PlacesDTO> places) {
+		List<PlacesDTO> itinerary = new ArrayList<>();
+		double bestDistance;
+		int posX = 0;
+		itinerary.add(places.get(0));
+		if (places.size() > 1) {
+			while (places.size() > 1) {
+				posX = 0;
+				bestDistance = 999999999999.00;
+				for (int i = 1; i < places.size(); i++) {
+					if (places.get(0).distanceTo(places.get(i)) < bestDistance) {					
+						bestDistance = places.get(0).distanceTo(places.get(i));
+						posX = i;
+					}else {
+						
+					}
+				}
+				itinerary.add(places.get(posX));
+				PlacesDTO trovato = places.get(posX);
+				places.remove(trovato);
+				places.set(0, trovato);
+				
+			}
+		}
+		
+		return itinerary;
+	}
+	
+	
+	
 	
 	//ALL crud methods in AbstractService
 }
