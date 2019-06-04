86b7af848022525570f0366acdb84a49f2304cf4
Matteo Faitanini <mfaitamf8@gmail.com>
2019-06-04 17:44:21 +0200

    modifica db

---
+++ db/newplaces.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT *FROM triplanrestnew.places
+LIMIT 0, 1000
+
+-- Date: 2019-06-04 17:40
+*/
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (1,'cultura','Roma',30,180,1,55.82,43.95,'Colosseo',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (2,'religione','Roma',22,60,1,52.03,42.45,'San Pietro',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (3,'divertimento','Roma',0,30,1,57.38,44.15,'Piazza di Spagna',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (4,'divertimento','Roma',15,120,2,55.69,46.85,'Pizzeria Da Nino',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (5,'divertimento','Roma',36,120,2,53.07,48.95,'Ristorante di Stokes',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (6,'festa','Roma',25,120,3,54.13,44.37,'PartyHouse',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (7,'divertimento','Amsterdam',15,90,4,100.33,37.9,'BullDog',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (8,'cultura','Amsterdam',10,120,1,10.12,35.34,'Van Gogh Museum',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (9,'divertimento','Amsterdam',0,30,4,103.06,33.27,'piazza Dam',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (10,'storia','Amsterdam',5,60,1,101.95,43.25,'Anna Frank house',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (11,'diverimento','Amsterdam',20,120,2,107.98,32.67,'Malj house',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (12,'festa','Amsterdam',25,120,3,105.56,31.26,'Disco',NULL);
+INSERT INTO `` (`idplaces`,`category`,`cityplaces`,`costo`,`durata`,`fascia`,`latitude`,`longitude`,`nameplaces`,`id_city`) VALUES (13,'sport','Amsterdam',22,60,1,105.23,31.35,'Amsterdam Arena',NULL);
