f73df684c2991e4ddeefa4b0bd8c94a733477626
Thehitman11 <alextontodonati@gmail.com>
2019-06-04 17:47:04 +0200

    modifica db

---
+++ db/image.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: Select* from image
+LIMIT 0, 1000
+
+-- Date: 2019-06-04 17:46
+*/
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (1,?,'cultura');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (2,?,'divertimento');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (3,?,'divertimento');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (4,?,'cultura');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (5,?,'divertimento');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (6,?,'divertimento');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (7,?,'cultura');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (8,?,'divertimento');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (9,?,'cultura');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (10,?,'divertimento');
+INSERT INTO `` (`id_image`,`image`,`type`) VALUES (11,?,'cultura');
