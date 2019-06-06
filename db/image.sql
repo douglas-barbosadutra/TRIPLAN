fe3f857cb8c1b01eb2282fdc8608bdc25386638c
Matteo Faitanini <mfaitamf8@gmail.com>
2019-06-06 09:46:35 +0200

    modifica

--- db/image.sql
+++ db/image.sql
@@ -1,19 +1,13 @@
-/*
--- Query: Select* from image
-LIMIT 0, 1000
+TRUNCATE TABLE image;
 
--- Date: 2019-06-04 17:46
-*/
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (1,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\chiesa.jpg','cultura');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (2,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\auto.jpg','divertimento');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (3,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\birra.jpg','divertimento');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (4,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\cruciverba.jpg','cultura');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (5,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\discoteca.jpg','divertimento');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (6,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\drink.jpg','divertimento');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (7,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\libro.jpg','cultura');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (8,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\MR.jpg','divertimento');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (9,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\quadro.jpg','cultura');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (10,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\ragazza.jpg','divertimento');
-INSERT INTO `` (`id_image`,`image`,`type`) VALUES (11,'C:\Users\39334\Documents\CONTRADER\TRIPLANRestNew\img\spaghetti.jpg','cultura');
-
-Select * from triplanrestnew.image;
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (1,'https://i.ibb.co/8mZdVxF/chiesa.jpg','cultura');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (2,'https://i.ibb.co/XVWS6Fc/auto.jpg','divertimento');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (3,'https://i.ibb.co/WtVqSKJ/birra.jpg','divertimento');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (4,'https://i.ibb.co/3pSHYH4/cruciverba.jpg','cultura');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (5,'https://i.ibb.co/tcJcmf6/discoteca.jpg','divertimento');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (6,'https://i.ibb.co/Yf1N6kV/drink.jpg','divertimento');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (7,'https://i.ibb.co/1Qt33hF/libro.jpg','cultura');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (8,'https://i.ibb.co/rZZxsNs/MR.jpg','divertimento');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (9,'https://i.ibb.co/GprhLzS/quadro.jpg','cultura');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (10,'https://i.ibb.co/1Gm4SVD/ragazza.jpg','divertimento');
+INSERT INTO `image` (`id_image`,`image`,`type`) VALUES (11,'https://i.ibb.co/k99F1SH/spaghetti.jpg','cultura');
